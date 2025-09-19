6c5accc0dc63cd7ec718a9c3a4da8f02a1c88389
Driftersha <Anyuta.bossenko@gmail.com>
2025-09-19 20:33:08 +0500

    feat: правки по макету и ТЗ

---
+++ src/components/AccountsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Учетные записи</h2>
+    <button @click="store.addAccount">+</button>
+
+    <AccountForm
+      v-for="acc in store.accounts"
+      :key="acc.id"
+      :account="acc"
+      @update="store.updateAccount"
+      @remove="store.removeAccount"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAccountsStore } from '@/stores/accounts';
+import AccountForm from './AccountForm.vue';
+
+const store = useAccountsStore();
+</script>
---
+++ src/stores/accounts.ts
@@ -0,0 +1,38 @@
+import { defineStore } from 'pinia'
+
+export type AccountType = 'LDAP' | 'Локальная'
+
+export interface Account {
+  id: number
+  labels: { text: string }[]
+  type: AccountType
+  login: string
+  password: string | null
+}
+
+export const useAccountsStore = defineStore('accounts', {
+  state: () => ({
+    accounts: [] as Account[],
+    nextId: 1,
+  }),
+  actions: {
+    addAccount() {
+      this.accounts.push({
+        id: this.nextId++,
+        labels: [],
+        type: 'Локальная',
+        login: '',
+        password: '',
+      })
+    },
+    removeAccount(id: number) {
+      this.accounts = this.accounts.filter(acc => acc.id !== id)
+    },
+    updateAccount(account: Account) {
+      const index = this.accounts.findIndex(a => a.id === account.id)
+      if (index !== -1) this.accounts[index] = account
+    },
+  },
+  persist: true,
+})
+
